003a16818efe63bcc1b251b33a17ad24958db44d
samhas <sami.hassini@bwi.de>
2022-01-31 12:30:19 +0100

    test dash app

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+import dash
+import dash_bootstrap_components as dbc
+import pandas as pd
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+from sent2vec.vectorizer import Vectorizer
+vectorizer = Vectorizer()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+app = dash.Dash(__name__, suppress_callback_exceptions=True,
+                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}])
+app.title = 'Auswertung und Analyse von Umfragen'
+
+
+server = app.server
