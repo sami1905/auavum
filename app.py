321a834c20c9d3bc07d1b8e3f8f1439b4ec7d4ae
samhas <sami.hassini@bwi.de>
2022-01-31 15:42:21 +0100

    integrate simple dash app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from dash_app import create_dash_app
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1>Wartungsmodus!</h1><p>Auf der Website finden zur Zeit Wartungsarbeiten statt! Die Website ist deshalb bis auf weiteres nicht verfügbar.</p>"
+create_dash_app(app)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
