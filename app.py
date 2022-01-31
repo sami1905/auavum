057bc14a9f5ffd8c35cd896e11465ee180294b41
samhas <sami.hassini@bwi.de>
2022-01-31 12:31:47 +0100

    delete sentence2vec

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-from sent2vec.vectorizer import Vectorizer
-vectorizer = Vectorizer()
+
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP],
