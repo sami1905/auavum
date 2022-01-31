8f5c671529dcd28f139a96b854300a52d9c32ecf
samhas <sami.hassini@bwi.de>
2022-01-31 12:49:09 +0100

    test

--- app.py
+++ app.py
@@ -10,5 +10,19 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True,
                             'content': 'width=device-width, initial-scale=1.0'}])
 app.title = 'Auswertung und Analyse von Umfragen'
 
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+
+app.layout = dbc.Container([
+    
+    html.Div(id='max-screen', 
+            children=[
+                html.H1('Wartungsarbeiten!', style={'font-size':'100px', 'margin':'40px'}),
+                html.H2('Die Anwendung steht in kürze bereit.', style={'font-size':'30px', 'margin':'30px'}),
+            ], className='max-screen'),
+
+            
+], className='content', fluid=True)
 
 server = app.server
--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,5 @@
 from app import app
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
 
-app.layout = dbc.Container([
-    
-    html.Div(id='max-screen', 
-            children=[
-                html.H1('Wartungsarbeiten!', style={'font-size':'100px', 'margin':'40px'}),
-                html.H2('Die Anwendung steht in kürze bereit.', style={'font-size':'30px', 'margin':'30px'}),
-            ], className='max-screen'),
-
-            
-], className='content', fluid=True)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
