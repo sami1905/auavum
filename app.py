fa824c7b926ee53f63d65425e9aa21d235b56d2e
samhas <sami.hassini@bwi.de>
2022-01-31 12:52:54 +0100

    back to working app

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-import dash
-import dash_bootstrap_components as dbc
-import pandas as pd
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "<h1>Wartungsmodus!</h1><p>Auf der Website finden zur Zeit Wartungsarbeiten statt! Die Website ist deshalb bis auf weiteres nicht verfügbar.</p>"
 
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.BOOTSTRAP],
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}])
-app.title = 'Auswertung und Analyse von Umfragen'
-
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
-
-app.layout = dbc.Container([
-    
-    html.Div(id='max-screen', 
-            children=[
-                html.H1('Wartungsarbeiten!', style={'font-size':'100px', 'margin':'40px'}),
-                html.H2('Die Anwendung steht in kürze bereit.', style={'font-size':'30px', 'margin':'30px'}),
-            ], className='max-screen'),
-
-            
-], className='content', fluid=True)
-
-server = app.server
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
