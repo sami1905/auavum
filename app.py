2490f89d2ab1fa89d25dba8ca408e6356ee03073
samhas <sami.hassini@bwi.de>
2022-01-31 17:48:39 +0100

    flask + dash

--- app.py
+++ app.py
@@ -1,49 +1,13 @@
 from flask import Flask
-#from dash_app import create_dash_app
-import dash
-import dash_bootstrap_components as dbc
+from dash_app import create_dash_app
 
 
-#app = Flask(__name__)
-
-server = Flask('app')
-app = dash.Dash(__name__, server=server,
-                external_stylesheets=[dbc.themes.BOOTSTRAP],
-                meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
-app.title = 'Auswertung und Analyse von Umfragen'
+app = Flask(__name__)
 
 
-#create_dash_app(app)
-  
-from app import app
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
-
-app.layout = dbc.Container([
-        # dcc.Location(id='url', refresh=False),
-        # dcc.Store(id='main_data', storage_type='session'),
-        # dcc.Store(id='main_data_after_preperation', storage_type='session'),
-        # dcc.Store(id='listOfFrei', storage_type='session'),
-        # dcc.Store(id='listOfFest', storage_type='session'),
-        html.Div(id='page-content', children=[], className='page-content'),
-        html.Div(id='max-screen', 
-                children=[
-                    html.H1('Das ist die Dash-App!', style={'font-size':'100px', 'margin':'40px'}),
-                    #html.Img(src='../assets/img/max_size.png', height='400px'),
-                    html.H2('Staaaaart!', style={'font-size':'30px', 'margin':'30px'}),
-                    html.P('Yeeah.',
-                className='card-text1')], className='max-screen'),
-
-                
-    ], className='content', fluid=True)
-@app.route("/")
-def hello():
-    return "<h1>Wartungsmodus!</h1><p>Auf der Website finden zur Zeit Wartungsarbeiten statt!Die Website ist deshalb bis auf weiteres nicht verfügbar.</p>"
 
 create_dash_app(app)
 
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
