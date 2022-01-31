24e072f34634c1ab049772cae32beabcdc53b3ab
samhas <sami.hassini@bwi.de>
2022-01-31 17:19:30 +0100

    integrate the dash app without flask

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
-from dash_app import create_dash_app
+# from flask import Flask
+# from dash_app import create_dash_app
+import dash
+import dash_bootstrap_components as dbc
+#app = Flask(__name__)
+app = dash.Dash(__name__, suppress_callback_exceptions=True,
+                 external_stylesheets=[dbc.themes.BOOTSTRAP],
+                 meta_tags=[{'name': 'viewport',
+                             'content': 'width=device-width, initial-scale=1.0'}])
+app.title = 'Auswertung und Analyse von Umfragen'
 
 
-app = Flask(__name__)
-
-create_dash_app(app)
 
   
+#create_dash_app(app)
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+server = app.server
--- index.py
+++ index.py
@@ -1,4 +1,34 @@
 from app import app
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# dash app
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+
+
+    
+app.layout = dbc.Container([
+        # dcc.Location(id='url', refresh=False),
+        # dcc.Store(id='main_data', storage_type='session'),
+        # dcc.Store(id='main_data_after_preperation', storage_type='session'),
+        # dcc.Store(id='listOfFrei', storage_type='session'),
+        # dcc.Store(id='listOfFest', storage_type='session'),
+        html.Div(id='page-content', children=[], className='page-content'),
+        html.Div(id='max-screen', 
+                children=[
+                    html.H1('Das ist die Dash-App!', style={'font-size':'100px', 'margin':'40px'}),
+                    #html.Img(src='../assets/img/max_size.png', height='400px'),
+                    html.H2('Staaaaart!', style={'font-size':'30px', 'margin':'30px'}),
+                    html.P('Yeeah.',
+                className='card-text1')], className='max-screen'),
+
+                
+], className='content', fluid=True)
+    
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
