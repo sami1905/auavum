b2ab779bf9a9fd9fff6928397294b201ccc29cd1
samhas <sami.hassini@bwi.de>
2022-02-06 23:12:54 +0100

    timeout to 2 sec

--- apps/method_lobby.py
+++ apps/method_lobby.py
@@ -244,7 +244,7 @@ layout = dbc.Container([
             ),
         ],
         id='modal-backdrop',
-        is_open=False,
+        is_open=True,
         backdrop='static',
         size='xl'
     ),
@@ -360,7 +360,7 @@ def update_lists(click, freitext_value, festtext_value, listOfFrei, listOfFest, 
             Input('listOfFest', 'data')],
             State('main_data_after_preperation', 'data'))
 def toggle_modal(click, listOfFrei, listOfFest, data):
-    time.sleep(5)
+    time.sleep(2)
     if data is None:
         return False
     elif click or listOfFrei != None or listOfFest != None:
